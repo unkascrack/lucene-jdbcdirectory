06372f5d55c996f1acd39b65048bf1af9e0ed8eb
Carlos Alonso <carlos.alonso.gonzalez@gmail.com>
2015-09-02 14:09:44 +0200

    revisión de la configuración del proyecto para añadir test unitarios y de integración.

---
+++ src/integration-test/java/com/github/lucene/jdbc/store/index/FetchOnOpenInputRAMAndFileOutputBelowThresholdITest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2004-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.lucene.jdbc.store.index;
+
+import org.apache.lucene.store.IndexInput;
+import org.apache.lucene.store.IndexOutput;
+
+/**
+ * @author kimchy
+ */
+public class FetchOnOpenInputRAMAndFileOutputBelowThresholdITest extends AbstractIndexInputOutputITest {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        jdbcDirectory.getSettings().getDefaultFileEntrySettings()
+                .setLongSetting(RAMAndFileJdbcIndexOutput.INDEX_OUTPUT_THRESHOLD_SETTING, 100);
+    }
+
+    @Override
+    protected Class<? extends IndexInput> indexInputClass() {
+        return FetchOnOpenJdbcIndexInput.class;
+    }
+
+    @Override
+    protected Class<? extends IndexOutput> indexOutputClass() {
+        return RAMAndFileJdbcIndexOutput.class;
+    }
+}
